d81d03762fb189a94709092221cfafb135588c3b
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-01-15 22:59:38 +0530

    Delete readingfromfile.py

--- readingfromfile.py
+++
@@ -1,9 +0,0 @@
-#reading data from the file in the output window
-
-filename=input("Enter the file name: ") #taking the filename from the user (type the filename including the extension (ex: apps.txt))
-
-f=open(filename, "r")
-data=f.read()
-print(type(data))#printing the type of the data
-print(data)
-f.close()
