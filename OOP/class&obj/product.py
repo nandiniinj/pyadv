70148d0258f61142f7b6c847c960bb394a25089f
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-02-15 22:32:22 +0530

    Add files via upload

---
+++ OOP/class&obj/product.py
@@ -0,0 +1,13 @@
+class Product:
+
+    def __init__ (self):
+        
+        self.Pid=""
+        self.Pname=""
+        self.Pcostprice=0.0
+        self.Psellingprice=0.0
+        self.Margin=0.0
+        self.Remarks=""
+        
+    def SetCourse (self):
+        
